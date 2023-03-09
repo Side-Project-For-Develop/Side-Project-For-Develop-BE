80ccf66c59a10b58861362f679c43c12d762ce95
teabear12 <118160987+teabear12@users.noreply.github.com>
2023-03-09 18:24:02 +0900

    first commit for sideproject

---
+++ src/main/java/com/h10/sideproject/SideprojectApplication.java
@@ -0,0 +1,13 @@
+package com.h10.sideproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SideprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/h10/sideproject/SideprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h10.sideproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
